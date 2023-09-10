104681252e243c741cef4d142ed324a4ae923eb9
Mario Gray <mario.gray@gmail.com>
2023-09-10 23:08:55 +0200

    take that typescript

---
+++ customerclient/src/main/java/com/example/customerclient/GatewayClientApplication.java
@@ -0,0 +1,41 @@
+package com.example.customerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayClientApplication.class, args);
+	}
+
+
+
+	// api:8081
+	// static-react-angular-vue-nginx:8020
+	@Bean
+	RouteLocator gateway(RouteLocatorBuilder rlb) {
+		var apiPrefix = "/api/";
+		return rlb
+				.routes()
+				// <1>
+				.route(rs -> rs
+						.path(apiPrefix + "**")
+						.filters(f -> f
+								.tokenRelay()
+								.rewritePath(apiPrefix + "(?<segment>.*)", "/$\\{segment}")
+						)
+						.uri("http://localhost:8081"))
+				// <2>
+				.route(rs -> rs
+						.path("/**")
+						.uri("http://localhost:8020")// cdn for static assets
+				)
+				.build();
+	}
+}
+
--- customerclient/src/test/java/com/example/customerclient/GatewayClientApplicationTests.java
+++ customerclient/src/test/java/com/example/customerclient/GatewayClientApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomerclientApplicationTests {
+class GatewayClientApplicationTests {
 
 	@Test
 	void contextLoads() {
